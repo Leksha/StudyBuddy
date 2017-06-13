bcb45f85fdbb2526457cae1e3d69976c0fd44561
Leksha <lramdene@uwaterloo.ca>
2017-06-12 21:57:10 -0400

    Created user profile info class

--- StudyBuddy/app/src/main/java/uw/studybuddy/UserProfileActivity.java
+++ StudyBuddy/app/src/main/java/uw/studybuddy/UserProfileActivity.java
@@ -2,12 +2,25 @@ package uw.studybuddy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class UserProfileActivity extends AppCompatActivity {
+
+    private TextView mUserDisplayName;
+    private TextView mUserName;
+    private TextView mUserCourses;
+    private TextView mUserAboutMe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_profile);
+
+        mUserDisplayName = (TextView)findViewById(R.id.user_profile_display_name);
+        mUserName = (TextView)findViewById(R.id.user_profile_name);
+        mUserCourses = (TextView)findViewById(R.id.user_profile_courses);
+        mUserAboutMe = (TextView)findViewById(R.id.user_profile_about_me);
+
+
     }
 }
---
+++ StudyBuddy/app/src/main/java/uw/studybuddy/userProfileInfo.java
@@ -0,0 +1,8 @@
+package uw.studybuddy;
+
+/**
+ * Created by leksharamdenee on 2017-06-12.
+ */
+
+public class userProfileInfo {
+}
