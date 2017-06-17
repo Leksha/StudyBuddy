0e5bdca467bd90813005dbb9a2e1a5dd62c46e45
Qinyuan Chen <a@AtekiMacBook-Air.local>
2017-06-17 19:24:28 -0400

    error message and email confirmation

--- StudyBuddy/app/src/main/java/uw/studybuddy/RegisterActivity.java
+++ StudyBuddy/app/src/main/java/uw/studybuddy/RegisterActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.view.menu.ExpandedMenuView;
+import android.text.TextUtils;
 import android.text.method.HideReturnsTransformationMethod;
 import android.text.method.PasswordTransformationMethod;
 import android.view.View;
@@ -12,11 +13,13 @@ import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class RegisterActivity extends AppCompatActivity {
 
@@ -48,21 +51,70 @@ public class RegisterActivity extends AppCompatActivity {
 
     public void SetupAccount(View view) {
         mAuth = FirebaseAuth.getInstance();
-        EditText Email = (EditText)findViewById(R.id.etUsernameReg);
+        final EditText Email = (EditText)findViewById(R.id.etUsernameReg);
         EditText Password = (EditText)findViewById(R.id.etPasswordReg);
+        EditText Auther = (EditText) findViewById(R.id.etConfirmationEmail) ;
+        final TextView Error = (TextView)findViewById(R.id.RegisterErrorDisplay);
         String email = Email.getText().toString();
         String password = Password.getText().toString();
+        final String Authemail = Auther.getText().toString();
+        if(TextUtils.isEmpty(email)){
+            String message = this.getString(R.string.EmptyEmail);
+            Email.setHint(message);
+            Email.setHintTextColor(getResources().getColor(R.color.errorhint));
+            return;
+        }
+        if(TextUtils.isEmpty(password)){
+            String message = this.getString(R.string.EmptyPassword);
+            Password.setHint(message);
+            Password.setHintTextColor(getResources().getColor(R.color.errorhint));
+            return;
+
+        }
+        if(TextUtils.isEmpty(Authemail)){
+            String message = this.getString(R.string.EmptyEmail);
+            Auther.setHint(message);
+            Auther.setHintTextColor(getResources().getColor(R.color.errorhint));
+            return;
+
+        }
+
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
                         if(!task.isSuccessful()){
-                            startActivity(new Intent(RegisterActivity.this, RegisterActivity.class));
+                            Error.setText(getResources().getString(R.string.email_account_already_registered));
+                            return;
+
+                        }else{
+                            //then do the authermail now
+                            SentConfirmation(Authemail);
+
+                        }
+                    }
+                });
+    }
+
+    public void SentConfirmation(String email) {
+        final FirebaseUser user = mAuth.getCurrentUser();
+        final EditText Auther = (EditText) findViewById(R.id.etConfirmationEmail) ;
+        final TextView Error = (TextView)findViewById(R.id.RegisterErrorDisplay);
+        user.sendEmailVerification()
+                .addOnCompleteListener(this, new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if(!task.isSuccessful()){
+                            String message = getResources().getString(R.string.InvalidEmail);
+                            Error.setText(message);
+                            return;
                         }else{
                             startActivity(new Intent(RegisterActivity.this, SetUpProfile.class));
                         }
                     }
                 });
+
+
     }
 
 }
--- StudyBuddy/app/src/main/java/uw/studybuddy/ResetPassword.java
+++ StudyBuddy/app/src/main/java/uw/studybuddy/ResetPassword.java
@@ -4,8 +4,10 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
@@ -22,15 +24,25 @@ public class ResetPassword extends AppCompatActivity {
 
     public void SentResetEmail(View view) {
         FirebaseAuth auth = FirebaseAuth.getInstance();
-        EditText Email = (EditText)findViewById(R.id.ResetEmail);
+        final EditText Email = (EditText)findViewById(R.id.ResetEmail);
         String email = Email.getText().toString();
+        final String message = this.getString(R.string.EmptyEmail);
 
+        final TextView Error = (TextView)findViewById(R.id.resentError);
+        if(TextUtils.isEmpty(email)){
+            Email.setHint(message);
+            Email.setHintTextColor(getResources().getColor(R.color.errorhint));
+            return;
+        }
         auth.sendPasswordResetEmail(email)
                 .addOnCompleteListener(new OnCompleteListener<Void>() {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()) {
                             startActivity(new Intent(ResetPassword.this, LoginActivity.class));
+                        }else{
+                            Error.setText(getResources().getString(R.string.Email_does_not_exist));
+                            return;
                         }
                     }
                 });
