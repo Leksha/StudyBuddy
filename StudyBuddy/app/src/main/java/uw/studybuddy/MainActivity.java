2e815d528c4be7db45487bd664c77cae5f43c333
Leksha <lramdene@uwaterloo.ca>
2017-06-20 11:23:27 -0400

    Working on DisplayCourses fragment

--- StudyBuddy/app/src/main/java/uw/studybuddy/MainActivity.java
+++ StudyBuddy/app/src/main/java/uw/studybuddy/MainActivity.java
@@ -56,7 +56,7 @@ public class MainActivity extends AppCompatActivity
         // Setup the fragment to be displayed
         Fragment fragment = null;
         Class fragmentClass = null;
-        fragmentClass = FindFriends.class;
+        fragmentClass = HomePage.class;
         try {
             fragment = (Fragment)fragmentClass.newInstance();
         } catch (Exception e) {
