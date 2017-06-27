9849530e80244d673d972317f8b6b0d7754c94b7
Leksha <lramdene@uwaterloo.ca>
2017-06-27 16:47:37 -0400

    Able to get userInfo

--- StudyBuddy/app/src/main/java/uw/studybuddy/UserProfile/UserProfileFragment.java
+++ StudyBuddy/app/src/main/java/uw/studybuddy/UserProfile/UserProfileFragment.java
@@ -4,9 +4,12 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import uw.studybuddy.R;
 
@@ -20,6 +23,14 @@ import uw.studybuddy.R;
  * create an instance of this fragment.
  */
 public class UserProfileFragment extends Fragment {
+
+    private TextView mUserDisplayName;
+    private TextView mUserName;
+    private TextView mUserCourses;
+    private TextView mUserAboutMe;
+
+    private UserInfo user;
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -66,7 +77,122 @@ public class UserProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_user_profile, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_user_profile, container, false);
+
+        mUserDisplayName = (TextView)rootView.findViewById(R.id.user_profile_display_name);
+        mUserName = (TextView)rootView.findViewById(R.id.user_profile_name);
+        mUserCourses = (TextView)rootView.findViewById(R.id.user_profile_courses);
+        mUserAboutMe = (TextView)rootView.findViewById(R.id.user_profile_about_me);
+
+        // For the purpose of the demo, we will create a user to display
+        user = UserInfo.getInstance();
+
+        // Update the user profile view with the right user info
+        mUserDisplayName.setText(user.getDisplayName());
+        mUserName.setText(user.getName());
+        mUserCourses.setText(getCoursesString());
+        mUserAboutMe.setText(user.getAboutMe());
+
+        setListeners();
+        return rootView;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (user == null) {
+            user = UserInfo.getInstance();
+        }
+        mUserDisplayName.setText(user.getDisplayName());
+        mUserName.setText(user.getName());
+        mUserCourses.setText(getCoursesString());
+        mUserAboutMe.setText(user.getAboutMe());
+    }
+
+    // Process the way user courses is displayed
+    private String getCoursesString() {
+        if (user == null) {
+            return "";
+        }
+        String courses = "";
+        String[] userCourses = user.getCourses();
+        int length = userCourses.length;
+        for (int i=0; i<length; i++) {
+            courses += userCourses[i];
+            if (i<length-1) {
+                courses += ", ";
+            }
+        }
+        return courses;
+    }
+
+    private void setListeners() {
+
+        // Update display name
+        mUserDisplayName.addTextChangedListener(new TextWatcher() {
+            String currString;
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                currString = mUserDisplayName.getText().toString();
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (currString !=null && currString != mUserDisplayName.getText().toString()) {
+                    user.setDisplayName(mUserDisplayName.getText().toString());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+        mUserName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (mUserName.getText().toString() != user.getName()) {
+                    user.setName(mUserName.getText().toString());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+
+        mUserCourses.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (mUserCourses.getText().toString() != user.getCourses().toString()) {
+                    String[] newCourses = mUserCourses.getText().toString().split(", ");
+                    user.setCourses(newCourses);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
+
+        mUserAboutMe.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (mUserAboutMe.getText().toString() != user.getAboutMe()) {
+                    user.setAboutMe(mUserAboutMe.getText().toString());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {}
+        });
     }
 
     // TODO: Rename method, update argument and hook method into UI event
