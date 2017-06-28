4686b9fe9dede2762476098d1449db54589853b7
Leksha <lramdene@uwaterloo.ca>
2017-06-28 17:32:16 -0400

    Created CourseInfo and UWAPI

---
+++ StudyBuddy/app/src/main/java/uw/studybuddy/CourseInfo.java
@@ -0,0 +1,17 @@
+package uw.studybuddy;
+
+/**
+ * Created by leksharamdenee on 2017-06-27.
+ */
+
+public class CourseInfo {
+    private String mSubject;
+    private String mCatalogNumber;
+    private String mCourseId;
+
+    public CourseInfo(String subject, String number) {
+        mSubject = subject;
+        mCatalogNumber = number;
+    }
+
+}
---
+++ StudyBuddy/app/src/main/java/uw/studybuddy/UWAPI.java
@@ -0,0 +1,42 @@
+package uw.studybuddy;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+
+import java.net.URL;
+
+/**
+ * Created by leksharamdenee on 2017-06-27.
+ * UW API KEY ed2c25922f87ba9a013af94a813856ff
+ * https://github.com/uWaterloo/api-documentation#accessing-the-api
+ */
+
+public class UWAPI extends AsyncTask<Void, Void, String> {
+
+//    private String apiKey = "ed2c25922f87ba9a013af94a813856ff";
+//    private UWaterlooApi api;
+//
+//    public UWAPI() {
+//        try {
+//            api = new UWaterlooApi(apiKey);
+//        }
+//    }
+//
+    protected String doInBackground(Void... urls) {
+        return null;
+    }
+//
+//
+//    protected void onPreExecute() {}
+//
+//    @Override
+//    protected void onPostExecute(Long aLong) {
+//        super.onPostExecute(aLong);
+//    }
+//
+//    @Override
+//    protected void onProgressUpdate(Integer... values) {
+//        super.onProgressUpdate(values);
+//    }
+
+}
